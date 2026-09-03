fa5fa4e5cb2d9abcf6cf96ce26cb30234ddae888
chintu12694 <chintuk12694@gmail.com>
2026-09-03 16:33:13 +0530

    Added Students API

--- src/main/java/com/ganga/StudentService/controller/TestController.java
+++ src/main/java/com/ganga/StudentService/controller/TestController.java
@@ -7,13 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/test")
 @CrossOrigin("*")
 public class TestController {
-    @GetMapping("/Hello")
-    public ResponseEntity<?> Hello(){
-        return new ResponseEntity("Hello", HttpStatus.OK);
+    @GetMapping("/students")
+    public ResponseEntity<?>getStudents(){
+        Map<String,Integer> students=new HashMap<>();
+        students.put("Hari",22);
+        students.put("Giri",32);
+        students.put("Siri",24);
+        students.put("Jari",22);
+        students.put("Pari",25);
+        return new ResponseEntity<>(students,HttpStatus.OK);
     }
-}
+};
 
